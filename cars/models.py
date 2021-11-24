ddf4c96e4e6cedf864c9131471a3ff4fb21d5b2e
kim-hoontae <gnsxo9@gmail.com>
2021-11-25 00:30:38 +0900

    feat : models 작성

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Tire(models.Model):
+    width        = models.PositiveIntegerField()
+    aspect_ratio = models.PositiveIntegerField()
+    wheel_size   = models.PositiveIntegerField()
+    created_at   = models.DateTimeField(auto_now=True)
+    updated_at   = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'tires'
+
+class UserTire(models.Model):
+    user       = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    tire       = models.ForeignKey('Tire', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'user_tires'
